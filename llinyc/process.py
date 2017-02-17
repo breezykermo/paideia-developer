813a7d9c1c99e530d9cd91f53d9455dedad5082e
Lachlan Kermode <lachlankermode@live.com>
2017-02-16 19:19:00 -0500

    successful partitioning (v messy)

--- llinyc/process.py
+++ llinyc/process.py
@@ -7,6 +7,7 @@ Note how to generate different outputs modifying the header params.
 '''
 import csv
 import sys
+import math
 from random import shuffle
 
 # 6 sessions in total
@@ -16,8 +17,8 @@ from random import shuffle
 # 4 greek groups, 2 beginner, and 2 intermediate/advanced.
 
 len_args = len(sys.argv)
-assert (len_args == 3), "\n\nYou need exactly 2 arguments: you have "+str(len_args)+"." \
-  + "\n python process.py no_of_groups lang"
+assert (len_args == 4), "\n\nYou need exactly 3 arguments: you have "+str(len_args)+"." \
+  + "\n python process.py no_of_groups lang outfile.csv"
 
 
 # HEADER PARAMS
@@ -27,7 +28,7 @@ latinSkill = 'Experience with Spoken Latin'
 greekSkill = 'Knowledge of Ancient Greek'
 
 INPUT_FILENAME = 'data2017.csv'
-OUTPUT_FILENAME = 'sat_groups.csv'
+OUTPUT_FILENAME = sys.argv[3]
 NO_OF_GROUPS = int(sys.argv[1])
 LANGUAGE = sys.argv[2]
 
@@ -47,16 +48,26 @@ categories = {
 
 # process a list into subsets of n items
 # chunks_of: 'a list -> int -> 'a list list
-def chunks_of(l, n=NO_OF_GROUPS):
+def chunks_of(l, n):
   return [l[i:i + n] for i in range(0, len(l), n)]
 
 # process a list into n subsets
 # chunks: 'a list -> int -> 'a list list
-def chunks(l, n=NO_OF_GROUPS):
+def chunks(l, n):
   return  [l[i:i+(len(l)//n)] for i in range(n)]
 
 # generate n categorized chunks from intermediate data structure
-# splitByCategory: 'a Categories -> n -> 'a list list
+# split_by_category: 'a Categories -> 'a list list
+def split_by_category(cgs):
+  full = cgs['beginner'] + cgs['intermediate'] + cgs['advanced']
+  group_threshold = int(math.ceil(len(full) / NO_OF_GROUPS))
+  output = []
+  for level in cgs:
+    shuffle(cgs[level])
+    for chunk in chunks_of(cgs[level], group_threshold):
+      output.append(chunk)
+  return output
+
 
 # read from csv
 key_fields = []
@@ -66,8 +77,8 @@ skill = latinSkill if (LANGUAGE == 'latin') else greekSkill
 # separate input into classes
 with open(INPUT_FILENAME, 'r') as f:
   data = csv.DictReader(f)
+  key_fields = data.fieldnames
   for person in data:
-    key_fields = person.keys()
     if person[skill] == levels[0]:
       categories['beginner'].append(person)
     elif person[skill] == levels[1]:
@@ -82,15 +93,20 @@ with open(OUTPUT_FILENAME, 'w') as f:
   writer = csv.DictWriter(f,fieldnames=key_fields)
   # partition
   # print(len(categories['beginner']))
-  groups = chunks(categories['beginner'])
-  print(len(groups[0]))
-  # print(groups)
-  for group in groups:
-    # print(len(group))
+  # groups = chunks(categories['beginner'])
+  # print(len(groups[0]))
+  # # print(groups)
+  # for group in groups:
+  #   # print(len(group))
+  #   for person in group:
+  #     writer.writerow(person)
+  #   writer.writerow({})
+  for group in split_by_category(categories):
     for person in group:
       writer.writerow(person)
     writer.writerow({})
   
+print('Successfully generated '+OUTPUT_FILENAME)
 
 
 # TESTING
